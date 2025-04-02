c8643fdfc621100c5d612ca6a5b7a325d9e61649
Natalia Yakovleva <yakovlevane29@gmail.com>
2025-04-02 13:01:53 +0300

    added swiper

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Initialize Swiper
+
+const swiper = new Swiper('.swiper', {
+
+  effect: "fade",
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
